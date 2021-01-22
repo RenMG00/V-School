58f260f7b51fbc4ba7d1ff2e255e8d62793f948a
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-01-21 22:19:29 -0700

    Create main.js

---
+++ assignments/loops-and-arrays-practice/main.js
@@ -0,0 +1,53 @@
+
+// Part 1: How many office items?
+var officeItems = [
+    "stapler",
+    "monitor",
+    "computer",
+    "desk",
+    "lamp",
+    "computer",
+    "lamp",
+    "stapler",
+    "computer",
+    "computer"
+]
+let counter = 0;
+for (let i = 0; i < officeItems.length; i++) {
+   counter++;
+}
+
+console.log(counter);
+
+//Part 2: Too Old or Too Young?
+var people = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    }, {
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    }, {
+        name: "Cheryl",
+        age: 22,
+        gender: "female"
+    }, {
+        name: "Sam",
+        age: 30,
+        gender: "male"
+    }, {
+        name: "Suzy",
+        age: 4,
+        gender: "female"
+    }
+] 
+
+for (var i = 0; i < people.length; i++) {
+    if (people[i].age < 18) {
+        console.log("Not old enough!")
+    } else {
+        console.log("Old enough!")
+    }
+}
