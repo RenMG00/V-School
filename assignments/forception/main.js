3beac3a63a7f6b2f9feec4b48b1da46f906d0fc4
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-01-26 18:20:04 -0700

    Create main.js

---
+++ assignments/forception/main.js
@@ -0,0 +1,23 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+function forception(people, alphabet){
+    var alphaArray = [];
+
+    for(var i = 0; i < alphabet.length; i++) {
+        alphaArray[i] = alphabet[i].toUpperCase();
+    }
+
+    var peopleArray = [];
+
+    for(var i = 0; i < people.length; i++) {
+        peopleArray.push(people[i] + ":");
+        for (var k = 0; k < alphaArray.length; k++) {
+            peo9ipleArray.push(alphaArray[k]);
+        }
+    }
+    return peopleArray;
+}
+console.log(forception(people, alphabet));
+// Output: 
+["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
