3bda2d0a7b79d85d6d65bc8c3a6ccb06d44d4d95
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-02-01 15:33:15 -0700

    Update main.js

--- assignments/array-methods/main.js
+++ assignments/array-methods/main.js
@@ -1,27 +1,27 @@
-var fruit = ["banana", "apple", "orange", "watermelon"];
-var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+let fruit = ["banana", "apple", "orange", "watermelon"];
+let vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 
 vegetables.pop()
-console.log('vegetables:',vegetables)
+console.log('vegetables:', vegetables)
 
 fruit.shift()
 console.log('fruit:', fruit)
 
-var orangeIndex = fruit.indexOf("orange")
+let orangeIndex = fruit.indexOf("orange")
 console.log(orangeIndex)
 fruit.push(orangeIndex)
 console.log('fruit:', fruit)
 
-var vegLength = vegetables.length
+let vegLength = vegetables.length
 console.log(vegLength)
 vegetables.push(vegLength)
 console.log('vegetables:', vegetables)
 
-var food = fruit.concat(vegetables)
-console.log('food:',food)
+let food = fruit.concat(vegetables)
+console.log('food:', food)
 
 food.splice(4, 2);
-console.log('food:',food);
+console.log('food:', food);
 
 const reversed = food.reverse();
 // Careful: reverse is destructive -- it changes the original array.
