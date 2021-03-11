ae3dce93b0a08f65fccf488f359f5acafc2a0341
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-03-11 16:33:50 -0700

    Create Friend.js

---
+++ assignments/friends-with-pets/src/Friend.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Pet from "./Pet"
+import friends from "./friends"
+
+const Friend = (props) => {
+
+     const petList = props.pets.map(pet => <Pet key={pet.name} {...pet}/>)
+    return (
+        <div style = {{
+            marginTop: 5,
+            padding: 5,
+            border: "1px solid gray",
+            width: 300,
+            fontFamily: "cursive",
+            backgroundColor: "whitesmoke"
+        }}>
+            <p style = {{
+                paddingRight: 5,
+                fontWeight: "bold"
+            }}>{props.name}</p>
+            <p> Age: {props.age}</p>
+            <p style = {{
+                textDecoration: "underline"
+            }}>Pets:</p>
+            {petList}
+        </div>
+    )
+}
+
+
+
+export default Friend;
