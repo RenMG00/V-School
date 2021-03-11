f71930952486af5de8cbf670d46083be0db76937
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-03-11 16:31:27 -0700

    Update Footer.js

--- assignments/blog-props/src/Footer.js
+++ assignments/blog-props/src/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faTwitterSquare, faGithub} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <div className="footer">
             <div className="footerTop">
-                <a className="footerIcon" href="http://facebook.com">facebook</a>
-                <a className="footerIcon" href="http://twitter.com">twitter</a>
-                <a className="footerIcon" href="https://github.com">github</a>
+                <a className="footerIcon" href="http://facebook.com"><FontAwesomeIcon icon={faFacebook} /></a>
+                <a className="footerIcon" href="http://twitter.com"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+                <a className="footerIcon" href="https://github.com"><FontAwesomeIcon icon={faGithub} /></a>
             </div>
             <div className="footerBottom">
                 <p className="copyright">Copyright © Your Website 2019</p>
