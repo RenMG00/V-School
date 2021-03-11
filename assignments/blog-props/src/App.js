3d20820b709f3b4cd125550c483921c043430537
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-03-11 13:32:36 -0700

    Create App.js

---
+++ assignments/blog-props/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import BlogList from "./BlogList";
+
+const App = () => {
+
+
+  return (
+    <div>
+      <Header />
+      <BlogList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
