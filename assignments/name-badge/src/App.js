da4168ff0425c5814d8702b492280825b3980118
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-03-25 15:29:11 -0600

    Create App.js

---
+++ assignments/name-badge/src/App.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Form from "./Form";
+import BadgeList from "./BadgeList";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      birthPlace: "",
+      phone: "",
+      favFood: "",
+      aboutMe: "",
+      disabled: true,
+      badges: []
+    };
+  }
+
+  handleChange = e => {
+    console.log(e.target)
+    this.setState(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+    console.log(this.state.firstName)
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const badge = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      birthPlace: this.state.birthPlace,
+      phone: this.state.phone,
+      favFood: this.state.favFood,
+      aboutMe: this.state.aboutMe
+    };
+
+    this.setState(prevState => ({
+      firstName: "",
+      lastName: "",
+      email: "",
+      birthPlace: "",
+      phone: "",
+      favFood: "",
+      aboutMe: "",
+      disabled: false,
+      badges: [...prevState.badges, badge]
+    }))
+
+    console.log(badge)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Form
+          firstName={this.state.firstName}
+          lastName={this.state.lastName}
+          email={this.state.email}
+          birthPlace={this.state.birthPlace}
+          phone={this.state.phone}
+          favFood={this.state.favFood}
+          aboutMe={this.state.aboutMe}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          disabled={this.state.disabled}
+        />
+        <div>
+          <BadgeList badges={this.state.badges} />
+        </div>
+      </div>
+
+    )
+  }
+}
+export default App;
+
