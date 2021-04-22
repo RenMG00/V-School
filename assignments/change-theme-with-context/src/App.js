0290a2c90efdeb2f8c7307c5f3c156624d3bdf3c
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-04-22 15:41:02 -0600

    Create App.js

---
+++ assignments/change-theme-with-context/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Main from "./Main";
+import Button from "./Button";
+import Footer from "./Footer";
+
+
+function App() {
+  return (
+    <div>
+     <NavBar />
+     <Main />
+     <Button/>
+     <Footer />         
+    </div>
+  );
+}
+
+export default App;
