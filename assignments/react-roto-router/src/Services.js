044fbb74047e151861e1d07a18b729abc4321aa3
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-04-24 14:32:27 -0600

    Create Services.js

---
+++ assignments/react-roto-router/src/Services.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Services() {
+    return (
+        <div className="services">
+        <h1>Services Offered</h1>
+        <br/>
+        <ul>
+        <li>Plumbing</li>
+        <br/>
+        <li>Electrical</li>
+        <br/>
+        <li>Appliance Reliair</li>
+        <br />
+        <li>Yard Maintenance</li>
+        </ul>
+        </div>
+    )
+}
+
+export default Services
