256b8d1e98b9e4bd5316888a5e46a19a3e96c5c9
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-04-24 14:32:05 -0600

    Create Home.js

---
+++ assignments/react-roto-router/src/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Welcome to Roto Works</h1>
+            <img src="https://media.istockphoto.com/photos/work-tool-with-plan-picture-id504899590?k=6&m=504899590&s=612x612&w=0&h=wZUCLN2fogWZOlzLNSxDl_iEByYXrLigUcdEtARvyRs=" alt="roto"></img>
+        </div>
+    )
+}
+
+export default Home
