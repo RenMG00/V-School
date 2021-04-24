66b0e45e6807a5ce2a1a2428a54d64b743a440a4
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-04-24 14:31:57 -0600

    Create Footer.js

---
+++ assignments/react-roto-router/src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <div className="footer">
+        <h1>Published by Ren Gian 2021</h1>
+        </div>
+    )
+}
+
+export default Footer
