12517bc88a193867747a7b6cfcaf92b1c4b54edc
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-04-24 14:31:49 -0600

    Create About.js

---
+++ assignments/react-roto-router/src/About.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function About() {
+    return (
+        <div className="about">
+            <h1>About Roto</h1>
+            <p>Roto LLC 2021</p>
+            <p>Information on site soon to come..</p>
+        </div>
+    )
+}
+
+export default About
