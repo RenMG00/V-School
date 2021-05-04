9f702644f4bda46a699ba20ae950b0ac5f546080
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-05-04 13:37:10 -0600

    Create MovieDetails.js

---
+++ assignments/personal-react-app/src/pages/MovieDetails.js
@@ -0,0 +1,41 @@
+import React, { useContext } from "react"
+import { MovieContext } from "../components/DataManager"
+
+const MovieDetails = () => {
+
+    const { movies } = useContext(MovieContext)
+
+    const handleSave = () => {
+        
+        let movieArray = []
+
+        const movieObj = {
+            Title: movies.Title,
+            Poster: movies.Poster,
+            Genre: movies.Genre,
+            Rated: movies.Rated
+        }
+        movieArray.push(movieObj)
+        movieArray = movieArray.concat(JSON.parse(localStorage.getItem("movieArray")||'[]'))
+        localStorage.setItem("movieArray", JSON.stringify(movieArray))
+    }
+
+    return (
+        <div>
+            <h1>Movie Details</h1>
+            <div className="movieDetails">
+                <h2>{movies.Title} : </h2>
+                <p>{movies.Year}</p>
+                <p>{movies.Rated}</p>
+                <p>{movies.Released}</p>
+                <p>{movies.Runtime}</p>
+                <p>{movies.Genre}</p>
+            </div>
+            <img src={movies.Poster} />
+            <button className="saveButton" onClick={handleSave}>Save Movie</button>
+        </div>
+
+    )
+}
+
+export default MovieDetails
