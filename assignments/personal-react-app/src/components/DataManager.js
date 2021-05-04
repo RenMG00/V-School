cfaf668ce82a50170395b234fc61cd36507ab129
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-05-04 13:36:36 -0600

    Update DataManager.js

--- assignments/personal-react-app/src/components/DataManager.js
+++ assignments/personal-react-app/src/components/DataManager.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react"
+import React, {useState, useEffect } from "react"
 import axios from "axios"
 
 export const MovieContext = React.createContext()
